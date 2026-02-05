d5733d1d7d0bd7972d48def477b881746936abfb
Guilherme Couto <guilherme.ncouto85@gmail.com>
2026-02-05 20:47:12 +0000

    Desafio

--- Exercicios/Python/DIO - Formacao Python Fundamentals/05 - Iteradores e geradores/02_Iterador_exemplo2.py
+++ Exercicios/Python/DIO - Formacao Python Fundamentals/05 - Iteradores e geradores/02_Iterador_exemplo2.py
@@ -1,3 +1,5 @@
+import time
+
 class MeuIterador:
     def __init__(self, numeros: list[int]):
         self.numeros = numeros
@@ -14,7 +16,27 @@ class MeuIterador:
         except IndexError:
             raise StopIteration
 
+# Criando massa de dados (1 a 100000)
+dados = list(range(1, 100001))
+meu_iteravel = MeuIterador(dados)
 
+tempo_total_inicio = time.perf_counter()
 
-for i in MeuIterador([1, 2, 3, 4, 5]):
-    print(i)
+print(f"{'Valor':<10} | {'Tempo Iteração (s)':<20}")
+print("-" * 35)
+
+for i in meu_iteravel:
+    # Início do tempo da iteração individual
+    it_inicio = time.perf_counter()
+    
+    # Exibição do resultado
+    print(f"{i:<10}", end=" | ")
+    
+    it_fim = time.perf_counter()
+    print(f"{it_fim - it_inicio:.8f}")
+
+tempo_total_fim = time.perf_counter()
+tempo_total_execucao = tempo_total_fim - tempo_total_inicio
+
+print("-" * 35)
+print(f"TEMPO TOTAL DE EXECUÇÃO: {tempo_total_execucao:.6f} segundos")
--- Exercicios/Python/DIO - Formacao Python Fundamentals/05 - Iteradores e geradores/04_Gerador_exemplo02.py
+++ Exercicios/Python/DIO - Formacao Python Fundamentals/05 - Iteradores e geradores/04_Gerador_exemplo02.py
@@ -1,8 +1,41 @@
+import time
+
+# Versão com Gerador
 def meu_gerador(numeros: list[int]):
     for numero in numeros:
         yield numero * 2
-    
-    
 
-for i in meu_gerador(numeros=[6, 7, 8, 9, 10]):
-    print(i)
+# Criando massa de dados (1 a 100000)
+dados = list(range(1, 100001))
+gerador_instancia = meu_gerador(dados)
+
+print(f"{'Gerador':<10} | {'Tempo Iteração (s)':<20}")
+print("-" * 35)
+
+# Início da contagem total
+tempo_total_inicio = time.perf_counter()
+
+for i in gerador_instancia:
+    # Início do tempo da iteração individual
+    it_inicio = time.perf_counter()
+    
+    # Exibição do resultado
+    print(f"{i:<10}", end=" | ")
+    
+    it_fim = time.perf_counter()
+    print(f"{it_fim - it_inicio:.8f}")
+
+# Fim da contagem total
+tempo_total_fim = time.perf_counter()
+tempo_total_execucao = tempo_total_fim - tempo_total_inicio
+
+print("-" * 35)
+print(f"TEMPO TOTAL (GERADOR): {tempo_total_execucao:.6f} segundos")
+
+'''
+    Característica      Gerador (yield)                     Iterador (Classe)
+ Esforço de Código      Mínimo (algumas linhas)             Médio (precisa de estrutura de classe)
+           Memória      Excelente (um item por vez)         Excelente (um item por vez)
+      Reutilização      Uma única vez (esgota o gerador)    Pode ser reiniciado se você programar
+              Foco      Ação (O que fazer com o dado)       Estado (O que o objeto sabe/guarda)
+'''
