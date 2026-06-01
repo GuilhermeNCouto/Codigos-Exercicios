a1fd9b742d6f7bc0941f614ea8eaa1450ca55018
Guilherme Couto <guilherme.ncouto85@gmail.com>
2026-06-01 15:59:59 +0000

    Boaspraticas

--- Exercicios/Python/DIO - Formacao Python Fundamentals/07 - Manipulando Arquivos/05 - boas_praticas.py
+++ Exercicios/Python/DIO - Formacao Python Fundamentals/07 - Manipulando Arquivos/05 - boas_praticas.py
@@ -2,7 +2,16 @@ from pathlib import Path
 
 ROOT_PATH = Path(__file__).parent
 
-arquivo = open(ROOT_PATH/"lorem.txt", "r")
-arquivo.close()
+try:
+    with open(ROOT_PATH/"lorem.txt", "r") as arquivo: # Utilizando o with para garantir que se feche sozinho
+        print(arquivo.read(), end="\n\n") # Imprime todo o conteúdo do arquivo como uma única string
 
-print(arquivo.read())
+except IOError as exc:
+    print(f"Erro ao abrir o arquivo: {exc}") # Imprime uma mensagem de erro personalizada
+
+try:
+    with open(ROOT_PATH/"utf-8.txt", 'w', encoding="utf-8" ) as arquivo:
+        arquivo.write('Arquivo com encoding.')
+except IOError as exc:
+    pass
+
