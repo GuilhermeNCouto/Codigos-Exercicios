892334506106e9da9d6d2c7084fdb508de378b83
Guilherme Couto <guilherme.ncouto85@gmail.com>
2026-06-03 17:17:10 +0000

    ManipulandoCSV

--- Exercicios/Python/DIO - Formacao Python Fundamentals/07 - Manipulando Arquivos/06 - trabalhando_com_CSVs.py
+++ Exercicios/Python/DIO - Formacao Python Fundamentals/07 - Manipulando Arquivos/06 - trabalhando_com_CSVs.py
@@ -1,3 +1,36 @@
 import csv
+from pathlib import Path
 
-with open('example.csv', 'r', )
+ROOT_PATH = Path(__file__).parent
+
+'''
+try:
+    with open(ROOT_PATH / 'usuarios.csv', 'w', newline='', encoding='utf-8', ) as arquivo_csv:
+        escritor = csv.writer(arquivo_csv)
+        escritor.writerow(['id','Nome'])
+        escritor.writerow(['1','Guilherme'])
+        escritor.writerow(['2','Letícia'])
+
+except IOError as exc:
+    print(f"Erro ao criar o arquivo: {exc}") # Imprime uma mensagem de erro personalizada
+'''
+
+try:
+    with open(ROOT_PATH / 'usuarios.csv', 'r', newline='', encoding='utf-8') as arquivo_csv:
+        leitor = csv.reader(arquivo_csv)
+        for index, row in enumerate(leitor):
+            if index == 0:
+                continue
+            print(f'ID: {row[0]} \nNome: {row[1]}')
+
+except IOError as exc:
+    print(f"Erro ao criar o arquivo: {exc}") # Imprime uma mensagem de erro personalizada
+
+
+# try:
+#     with open(ROOT_PATH / 'usuarios.csv', 'r', newline='', encoding='utf-8') as arquivo_csv:
+#         leitor = csv.DictReader(arquivo_csv)
+#         for row in leitor:
+#             print(row['id'], row['Nome'])
+# except IOError as exc:
+#     print(f"Erro ao criar o arquivo: {exc}") # Imprime uma mensagem de erro personalizada
