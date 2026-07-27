a032947d04049eefea8533a07d3d0ee24e1cb481
Guilherme Couto <guilherme.ncouto85@gmail.com>
2026-07-27 18:48:47 +0000

    exercicio16

---
+++ Exercicios/Python/Curso em video/POO/Desafios/desafio016.py
@@ -0,0 +1,8 @@
+'''
+Crie a classe Funcionario, onde podemos cadastrar nome, setor e cargo.
+Crie também um método que permita ao funcionário se apresentar.
+'''
+
+class Funcionario:
+
+    def __init__(self):
