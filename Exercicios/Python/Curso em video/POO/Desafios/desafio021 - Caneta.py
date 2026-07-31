25baffdd5dff610f7c2ac294d455440efd12d1ee
Guilherme Couto <guilherme.ncouto85@gmail.com>
2026-07-31 19:57:56 +0000

    desafio022

--- Exercicios/Python/Curso em video/POO/Desafios/desafio021 - Caneta.py
+++ Exercicios/Python/Curso em video/POO/Desafios/desafio021 - Caneta.py
@@ -7,32 +7,43 @@ from rich import print
 from rich.panel import Panel
 
 class Caneta:
+    
+    MAPA_CORES = {
+        "azul": "blue",
+        "vermelha": "red",
+        "vermelho": "red",
+        "verde": "green"
+    }
+    
     def __init__(self, cor="default"):
-        cores = dict(azul="blue", vermelha="red", verde="green")
-        self.cor = cores.get(cor, "default")
+        self.cor = self.MAPA_CORES.get(cor.lower().strip(), "default")
         self.tampada = True
 
     def destampar(self):
         self.tampada = False
+        
+    def tampar(self):
+        self.tampada = True
 
     def escrever(self, frase):
         if not self.tampada:
             print(f"[{self.cor}]{frase}[/]", end=" ")
         else:
-            print("A caneta está tampada!")
+            print(f":prohibited: [{self.cor}]caneta[/] está tampada!")
 
     def quebrar_linha(self, num_linhas):
-        for linha in range(num_linhas):
-            print("")
+        print("\n" * num_linhas, end="")
+            
+            
 c1 = Caneta("azul")
-c2 = Caneta("vermelha")
+c2 = Caneta("vermelho")
 c3 = Caneta("verde")
 
 c1.destampar() 
 c2.destampar()
 c3.destampar()
 
-c1.escrever("Olá, tudo bem?")
+c1.escrever("Vamos nessa!")
 c1.quebrar_linha(2)
 c2.escrever("Olá, Gafanhoto!")
 c3.escrever("Vamos exercitar!")
--- Exercicios/Python/Curso em video/POO/Desafios/desafio022 - Controle.py
+++ Exercicios/Python/Curso em video/POO/Desafios/desafio022 - Controle.py
@@ -7,26 +7,88 @@ volume = "+ e -"
 liga/desliga = "@"
 '''
 
+import os
+from readchar import readchar
 from rich import print
 from rich.panel import Panel
 
 class ControleRemoto:
+    canal_min: int = 1
+    canal_max: int = 5
+    volume_min: int = 1
+    volume_max: int = 5
+
+    def __init__(self, canal = 1, volume = 2):
+        self.canal_atual: int = canal
+        self.volume_atual: int = volume
+        self.ligado: bool = False
     
-    def __init__(self):
-        self.canal = 1
-        self.volume = 5
-        self.ligado = True
-        return self.liga_desliga()
-
     def liga_desliga(self):
+        self.ligado = not self.ligado
+    
+    def aumentar_canal(self):
         if self.ligado:
-            self.ligado = False
-            print(Panel(":prohibited:[red] A TV está desligada![/]", title="[ TV ]", width=30))
+            if self.canal_atual == self.__class__.canal_max:
+                self.canal_atual = self.__class__.canal_min
+            else:
+                self.canal_atual += 1
+
+    def diminuir_canal(self):
+        if self.ligado:
+            if self.canal_atual == self.__class__.canal_min:
+                self.canal_atual = self.__class__.canal_max
+            else:
+                self.canal_atual -= 1
+    
+    def aumentar_volume(self):
+        if self.ligado:
+            if self.volume_atual != self.__class__.volume_max:
+                self.volume_atual += 1
+                
+    def diminuir_volume(self):
+        if self.ligado:
+            if self.volume_atual != self.__class__.volume_min:
+                self.volume_atual -= 1
+            
+    def mostrar_tv(self):
+        if not self.ligado:
+            conteudo = ":prohibited: [red]A TV está desligada[/]"
         else:
-            self.ligado = True
-            print(Panel(":tv:[green] A TV está ligada![/]", title="[ TV ]", width=30))
-
-    def tela(self):
-        tela = panel(f"[blue]Canal: {self.canal}[/]\n[green]Volume: {self.volume}[/]", title="[ TV ]", width=30)
-
+            conteudo = f"CANAL  = "
+            for canal in range(self.canal_min, self.canal_max + 1):
+                if canal == self.canal_atual:
+                    conteudo += f"[yellow on yellow] {canal} [/]"
+                else:
+                    conteudo += f"{canal} "
+                    
+            conteudo += "\nVOLUME = " + "[black on red] [/]" * self.volume_atual + "[black on white] [/]" * (self.volume_max - self.volume_atual)
+            
+        tv = Panel(conteudo, title="[ TV ]", width=30)
+        print(tv)
+        
 controle = ControleRemoto()
+
+while True:
+    # Limpa o terminal antes de desenhar a TV (funciona no Windows e Linux/Codespaces)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+    controle.mostrar_tv()
+    
+    print("\n[ @: Liga | <: Ch- | >: Ch+ | -: Vol- | +: Vol+ | 0: Sair ]\n")
+    
+    # Espera apenas 1 tecla e já joga direto para o match, sem pedir Enter!
+    comando = readchar()
+    
+    match comando:
+        case '0':
+            break
+        case '@':
+            controle.liga_desliga()
+        case '<':
+            controle.diminuir_canal()
+        case '>':
+            controle.aumentar_canal()
+        case '-':
+            controle.diminuir_volume()
+        case '+':
+            controle.aumentar_volume()
